4112e043c6f53e4869516fafe713010895714dac
ikbalzohr <ikbal.gf12@gmail.com>
2024-08-01 17:24:26 +0800

    setup nativewind

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,22 +1,26 @@
 import React, { useEffect } from "react";
 import { View, Text, Button, Alert } from "react-native";
-import { useAuthStore } from "@/lib/store/authStore";
+// import { useAuthStore } from "@/lib/store/authStore";
 import { Link } from "expo-router";
+import { styled } from "nativewind";
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
 
 const LoginScreen: React.FC = () => {
-  const { loginWithGoogle, signOut } = useAuthStore();
+  // const { loginWithGoogle, signOut } = useAuthStore();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <StyledView className="bg-blue-500" style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Login Screen</Text>
-      <Button title="Login with Google" onPress={loginWithGoogle} />
+      <Button title="Login with Google" onPress={() => {}} />
       <View style={{ marginTop: 20 }} />
-      <Button title="Logout" onPress={() => signOut()} />
+      <Button title="Logout" onPress={() => {}} />
       <View style={{ marginTop: 20 }} />
       <Link href="/home">
-        <Text>Home</Text>
+        <StyledText className="text-blue-600">Home</StyledText>
       </Link>
-    </View>
+    </StyledView>
   );
 };
 
